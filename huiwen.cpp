69694dd8f4f70dcca8c92afec2a3858d203bb58e
brazer_glb <andysl9@163.com>
2021-04-08 16:30:13 +0800

    添加项目文件。

---
+++ huiwen.cpp
@@ -0,0 +1,25 @@
+﻿#include<cstdio>
+#include<cstring>
+int main()
+{
+	char ans[90];
+	char cans[90][90];
+	int i;
+	int r=0, h=0;
+	gets_s(ans,89);
+	for (i = 0; i < 81; i++)
+	{
+		if (ans[i] != ' ')
+			cans[r][h++] = ans[i];
+		else if (ans[i] == ' ')
+		{
+			cans[r][h] = '\0';
+			h = 0;
+			r += 1;
+		}
+	}
+	for (i = r; i >= 0; i--)
+	{
+		printf("%s", cans[i]); if (i > 0)printf(" ");
+	}
+}
